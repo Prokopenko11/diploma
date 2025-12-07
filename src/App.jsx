b86e7bbcb41bad920b0596da01834615a9f6902f
Valeria Prokopenko <valeriaprokopenko59@gmail.com>
2025-12-07 22:04:08 +0300

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import ScrollToTop from './components/SrollToTop';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/diploma">
       <ScrollToTop />
       <div className="App">
         <Routes>
